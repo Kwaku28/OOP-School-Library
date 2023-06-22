e92afbed9b580d4d175b6737ed4888bcdd107386
Lawrence Amoafo <rekenburg777@gmail.com>
2023-06-22 08:59:37 +0000

    Update the test cases

--- test.rb
+++ test.rb
@@ -1,4 +1,7 @@
 require_relative 'person'
+require_relative 'decorator'
+require_relative 'trimmer'
+require_relative 'capitalize'
 
 person = Person.new(22, 'maximilianus')
 puts person.correct_name
