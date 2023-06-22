317f1bbc64b79bcc85b22065e0f01770e07c3bdf
Lawrence Amoafo <rekenburg777@gmail.com>
2023-06-22 08:59:14 +0000

    Move decorator to different file

--- person.rb
+++ person.rb
@@ -1,8 +1,4 @@
-class Nameable
-  def correct_name
-    raise NotImplementedError
-  end
-end
+require_relative 'nameable'
 
 class Person < Nameable
   attr_reader :id
@@ -29,30 +25,5 @@ class Person < Nameable
 
   def of_age?
     @age > 18
-  end
-end
-
-class Decorator < Nameable
-  attr_accessor :nameable
-
-  def initialize(nameable)
-    super()
-    @nameable = nameable
-  end
-
-  def correct_name
-    @nameable.correct_name
-  end
-end
-
-class CapitalizeDecorator < Decorator
-  def correct_name
-    @nameable.correct_name.capitalize
-  end
-end
-
-class TrimmerDecorator < Decorator
-  def correct_name
-    @nameable.correct_name[0, 10]
   end
 end
