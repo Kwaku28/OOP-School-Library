de7086250fcfb438a5aabc114fba69d73a24ed54
rushemaa <rushemaa@gmail.com>
2023-06-30 16:23:34 +0200

    Add Teacher tests

---
+++ spec/teacher_test_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../teacher'
+
+
+describe Teacher do
+  context 'Testing Teacher' do
+    it 'Should create a teacher' do
+      teacher = Teacher.new(56, 'Computer ingeneering', 'James', parent_permission: true)
+      expect(teacher).to be_a Teacher
+    end
+
+    it 'Should use services' do
+      teacher = Teacher.new(56, 'Computer ingeneering', 'James', parent_permission: true)
+      expect(teacher.can_use_services?).to be true
+    end
+
+  end
+end
